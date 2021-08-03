440c59e00da85c8ff84109163622806ef56a2001
carlos menjivar <carlos.itca@gmail.com>
2021-08-03 17:55:44 -0600

    :recycle: Refactor test to minimal file

--- spec/test.rb
+++ spec/test.rb
@@ -1,33 +1,25 @@
 describe 'database' do
-    before do
-        `rm -rf test.db`
+  def run_script(commands) 
+    raw_output = nil
+    IO.popen("./main", "r+") do |pipe|
+      commands.each do |command|
+        pipe.puts command
       end
-    
-      def run_script(commands)
-        raw_output = nil
-        IO.popen("./main test.db", "r+") do |pipe|
-          commands.each do |command|
-            begin
-              pipe.puts command
-            rescue Errno::EPIPE
-              break
-            end
-          end
-    
-          pipe.close_write
-    
-          # Read entire output
-          raw_output = pipe.gets(nil)
-        end
-        raw_output.split("\n")
-      end
-    
-      it 'inserts and retrieves a row' do
-        result = run_script([
-          ".exit",
-        ])
-        expect(result).to match_array([
-          "db > bye",
-        ])
-      end
+
+      pipe.close_write
+
+      # Read entire output
+      raw_output = pipe.gets(nil)
+    end
+    raw_output.split("\n")
+  end
+
+  it 'exit from database' do
+    result = run_script([
+      ".exit",
+    ])
+    expect(result).to match_array([
+      "db > bye",
+    ])
+  end
 end
