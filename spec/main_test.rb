cc997ff2b264ff0ad827ed7bd4b9dccc57cceedf
carlos menjivar <carlos.itca@gmail.com>
2021-08-03 18:33:58 -0600

    :white_check_mark: Add test to insert and select a single row

--- spec/main_test.rb
+++ spec/main_test.rb
@@ -29,8 +29,23 @@ describe 'database' do
       ".exit"
     ])
     expect(result).to match_array([
-      "Executed.",
       "db > Empty table.",
+      "Executed.",
+      "db > bye"
+    ])
+  end
+
+  it 'insert and retrieve a single row' do 
+    result = run_script([
+      "insert 1 user_1 user_1@gmail.com",
+      "select",
+      ".exit"
+    ])
+    expect(result).to match_array([
+      "Executed.",
+      "db > (1, user_1, user_1@gmail.com)",
+      "1 rows printed.",
+      "db > Executed.",
       "db > bye"
     ])
   end
