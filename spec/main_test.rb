232ecf18f0ce37263339e428940629aba00aa27a
carlos menjivar <carlos.itca@gmail.com>
2021-08-04 10:02:59 -0600

    :white_check_mark: Add test to verify when table is full

--- spec/main_test.rb
+++ spec/main_test.rb
@@ -49,4 +49,20 @@ describe 'database' do
       "db > bye"
     ])
   end
+
+  it 'prints error message when table is full' do
+
+    ##
+    # The max num of rows is 1401,
+    # the ruby script doesn't behave correcty with 1..1401 loop
+    # this prints less output than expected, so 1549 is the minimun
+    # number with with it works
+    #
+    script = (1..1549).map do |i|
+      "insert #{i} user#{i} user#{i}@example.com"
+    end
+    script << ".exit"
+    result = run_script(script)
+    expect(result[-2]).to eq('db > Error: Table full.')
+  end
 end
